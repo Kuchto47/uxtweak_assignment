16c9fe481e54d75d1754d120c09f75a0427fafc5
Kuchto47 <martin.k738@gmail.com>
2026-07-22 21:22:35 +0200

    make initial migration

---
+++ uxtweak_backend/src/migrations/1784747604273-Init.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class Init1784747604273 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "chatroom" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "name" character varying NOT NULL, CONSTRAINT "PK_9c4e4a89e3674fc9f382d733f03" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "message" ("id" SERIAL NOT NULL, "chatroom_fk_id" uuid NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "sender_name" character varying NOT NULL, "body" character varying NOT NULL, CONSTRAINT "PK_e0ef0550174fd1099a308fd18a0" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "message" ADD CONSTRAINT "FK_message_chatroom" FOREIGN KEY ("chatroom_fk_id") REFERENCES "chatroom"("id") ON DELETE CASCADE`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "message"`);
+    await queryRunner.query(`DROP TABLE "chatroom"`);
+  }
+}

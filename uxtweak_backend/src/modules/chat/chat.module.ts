a023a86f482733c17b0b43e92a7b42d61d574761
Kuchto47 <martin.k738@gmail.com>
2026-07-22 21:56:12 +0200

    edit module

--- uxtweak_backend/src/modules/chat/chat.module.ts
+++ uxtweak_backend/src/modules/chat/chat.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ChatroomEntity } from './model/entity/chatroom.entity';
-import { MessageEntity } from './model/entity/message.entity';
+import { Chatroom } from './model/entity/chatroom.entity';
+import { ChatMessage } from './model/entity/chatMessage.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ChatroomEntity, MessageEntity])],
+  imports: [TypeOrmModule.forFeature([Chatroom, ChatMessage])],
   controllers: [],
   providers: [/*router*/, /*service*/],
   exports: [TypeOrmModule],

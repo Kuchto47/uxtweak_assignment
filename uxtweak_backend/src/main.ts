46fdbe693de8aa2eee730ff435614a422db7db4f
Kuchto47 <martin.k738@gmail.com>
2026-07-24 09:12:57 +0200

    configure console logger for the app

--- uxtweak_backend/src/main.ts
+++ uxtweak_backend/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
+import { ConsoleLogger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: new ConsoleLogger({
+      colors: true,
+      prefix: 'UXTweakBE',
+    }),
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

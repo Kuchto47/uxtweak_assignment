61cd3ad7480ea5529872667079835253e3ea8454
Kuchto47 <martin.k738@gmail.com>
2026-07-24 16:22:13 +0200

    fix chat module wiring

--- uxtweak_backend/src/modules/chat/chat.module.ts
+++ uxtweak_backend/src/modules/chat/chat.module.ts
@@ -4,11 +4,12 @@ import { Chatroom } from './model/entity/chatroom.entity';
 import { ChatMessage } from './model/entity/chatMessage.entity';
 import { ChatRouter } from './chat.router';
 import { ChatService } from './chat.service';
+import { ChatMessageEventingService } from './chatMessageEventing.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Chatroom, ChatMessage])],
   controllers: [],
-  providers: [ChatRouter, ChatService],
+  providers: [ChatRouter, ChatService, ChatMessageEventingService],
   exports: [TypeOrmModule],
 })
 export class ChatModule {}

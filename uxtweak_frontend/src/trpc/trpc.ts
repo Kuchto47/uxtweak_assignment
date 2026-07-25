f9fc65cce009fd50f89c5e1a01f78de0be4c7cde
Kuchto47 <martin.k738@gmail.com>
2026-07-25 22:58:53 +0200

    add todo to move base api url to env

--- uxtweak_frontend/src/trpc/trpc.ts
+++ uxtweak_frontend/src/trpc/trpc.ts
@@ -1,7 +1,7 @@
 import { createTRPCClient, splitLink, httpBatchLink, httpSubscriptionLink } from '@trpc/client';
 import { type AppRouter } from '../../../uxtweak_backend/src/trpc/@generated/types';
 
-const BASE_API_URL = 'http://localhost:3030';
+const BASE_API_URL = 'http://localhost:3030'; // TODO env this
 
 export const trpcClient = createTRPCClient<AppRouter>({
   links: [

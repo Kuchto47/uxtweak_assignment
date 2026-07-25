b4b18c702e56596636f5d07e86dae57cc2341078
Kuchto47 <martin.k738@gmail.com>
2026-07-25 23:13:15 +0200

    make loading and error UI for chatrooms

--- uxtweak_frontend/src/pages/index/(index).vue
+++ uxtweak_frontend/src/pages/index/(index).vue
@@ -10,7 +10,33 @@
       />
 
       <div class="text-h6 q-mb-sm">Chatrooms available:</div>
-      <q-list bordered separator rounded>
+
+      <div v-if="isPending">
+        <q-list bordered separator rounded>
+          <q-item v-for="n in 3" :key="n">
+            <q-item-section>
+              <q-item-label>
+                <q-skeleton type="text" width="60%" />
+              </q-item-label>
+            </q-item-section>
+
+            <q-item-section side>
+              <q-skeleton type="QBtn" />
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </div>
+
+      <div v-else-if="isError">
+        <q-banner class="bg-negative text-white rounded-borders">
+          <template v-slot:avatar>
+            <q-icon name="error" color="white" />
+          </template>
+          Failed to load chatrooms. Please try again later.
+        </q-banner>
+      </div>
+
+      <q-list v-else bordered separator rounded>
         <q-item v-for="room in chatrooms" :key="room.id">
           <q-item-section>
             <q-item-label>{{ room.name }}</q-item-label>
@@ -29,8 +55,7 @@
 import { ref } from 'vue';
 import { useGetChatRooms } from '@/features/chatrooms/useGetChatRooms';
 
-const _chatRoomsFromServer = useGetChatRooms();
+const { isError, isPending, data: chatrooms } = useGetChatRooms();
 
 const nickname = ref('');
-const chatrooms = ref([{ id: 1, name: 'UXTweak Chatroom' }]);
 </script>

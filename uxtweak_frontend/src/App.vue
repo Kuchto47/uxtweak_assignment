dadeb9fc8025b2c7bf848ebe10ed470615b44659
Kuchto47 <martin.k738@gmail.com>
2026-07-26 08:46:58 +0200

    fix wiring of layout and pages

--- uxtweak_frontend/src/App.vue
+++ uxtweak_frontend/src/App.vue
@@ -1,3 +1,13 @@
 <template>
-  <router-view />
+  <q-layout view="lHh Lpr lFf">
+    <navbar />
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
 </template>
+
+<script setup lang="ts">
+import Navbar from '@/features/navbar/navbar.vue';
+</script>

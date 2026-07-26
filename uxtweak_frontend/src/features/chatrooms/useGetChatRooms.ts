5115589473d8082cd52d878ec12ab45d170a53cd
Kuchto47 <martin.k738@gmail.com>
2026-07-26 08:41:55 +0200

    refactor useGetChatRooms

--- uxtweak_frontend/src/features/chatrooms/useGetChatRooms.ts
+++ uxtweak_frontend/src/features/chatrooms/useGetChatRooms.ts
@@ -1,12 +1,16 @@
-import { useQuery } from '@tanstack/vue-query';
+import { queryOptions, useQuery } from '@tanstack/vue-query';
 import { trpcClient } from '@/trpc/trpc';
 
 const GET_CHATROOMS_QUERY_KEY = 'chatrooms';
 
-export const useGetChatRooms = () => {
-  return useQuery({
+export const getChatRoomsQueryOptions = () => {
+  return queryOptions({
     queryKey: [GET_CHATROOMS_QUERY_KEY],
     queryFn: () => trpcClient.chatRouter.getChatRooms.query(),
-  });
+  })
+}
+
+export const useGetChatRooms = () => {
+  return useQuery(getChatRoomsQueryOptions());
 }
 

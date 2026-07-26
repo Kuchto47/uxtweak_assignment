e5d2fbefbe6ffae84be1219851f442457a9b29bf
Kuchto47 <martin.k738@gmail.com>
2026-07-26 07:53:27 +0200

    prepare chatroom name showing in navbar

--- uxtweak_frontend/src/pages/index.vue
+++ uxtweak_frontend/src/pages/index.vue
@@ -4,6 +4,14 @@
       <q-toolbar>
         <q-toolbar-title> UXTweak Chat </q-toolbar-title>
 
+        <q-space />
+
+        <div v-if="activeChatroom">
+          {{ activeChatroom }}
+        </div>
+
+        <q-space />
+
         <div>v0.0.1</div>
       </q-toolbar>
     </q-header>
@@ -15,5 +23,10 @@
 </template>
 
 <script setup lang="ts">
-//
+import { ref } from 'vue';
+import { useQueryClient } from '@tanstack/vue-query'
+
+const _queryClient = useQueryClient()
+// TODO activeChatroom will be name of chatroom based on ID in URL
+const activeChatroom = ref('');
 </script>

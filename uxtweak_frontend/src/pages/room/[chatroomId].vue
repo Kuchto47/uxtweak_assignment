65542e7ca8d7c3aa798aaf631f8a58703d97aec7
Kuchto47 <martin.k738@gmail.com>
2026-07-26 10:16:16 +0200

    add initial chatroom messages

--- uxtweak_frontend/src/pages/room/[chatroomId].vue
+++ uxtweak_frontend/src/pages/room/[chatroomId].vue
@@ -9,8 +9,27 @@
         paddingBottom: '40px',
       }"
     >
-      <div id="chatmessagescontainer" class="col scroll q-pa-md">
-        <!-- Messages will go here -->
+      <div id="chatmessagescontainer" ref="scrollContainer" class="col scroll q-pa-md">
+        <template v-if="isPending">
+          <ChatroomSkeleton />
+        </template>
+
+        <template v-else-if="isError">
+          <ChatroomMessagesLoadingError />
+        </template>
+
+        <template v-else-if="messages">
+          <q-chat-message
+            v-for="msg in messages"
+            :key="msg.id"
+            :name="msg.senderName !== nickname ? msg.senderName : undefined"
+            :stamp="formatDate(msg.timestamp)"
+            :text="[msg.message]"
+            :sent="msg.senderName === nickname"
+            :bg-color="msg.senderName === nickname ? 'primary' : 'grey-4'"
+            :text-color="msg.senderName === nickname ? 'white' : 'black'"
+          />
+        </template>
       </div>
 
       <div class="q-px-md">
@@ -26,11 +45,33 @@
 
 <script setup lang="ts">
 import { useRoute } from 'vue-router';
-import { ref } from 'vue';
-import { useGetRecentMessagesForRoom } from '@/features/chatrooms/useGetRecentMessagesForRoom';
+import { nextTick, ref, watch } from 'vue';
+import { useGetRecentMessagesForRoom } from '@/features/chatrooms/api/useGetRecentMessagesForRoom';
+import { useNickname } from '@/features/chatrooms/utils/useNickname';
+import ChatroomSkeleton from '../../features/chatrooms/components/ChatroomSkeleton.vue';
+import ChatroomMessagesLoadingError from '../../features/chatrooms/components/ChatroomMessagesLoadingError.vue';
+import { formatDate } from '@/features/chatrooms/utils/formatDate';
 
 const route = useRoute('/room/[chatroomId]');
-const { isPending: _, isError: __, data: _messages } = useGetRecentMessagesForRoom(route.params.chatroomId);
+const { isPending, isError, data: messages } = useGetRecentMessagesForRoom(route.params.chatroomId);
+const { nickname } = useNickname();
 
 const message = ref('');
+
+const scrollContainer = ref<HTMLElement | null>(null);
+
+const scrollToBottom = async () => {
+  await nextTick();
+  if (scrollContainer.value) {
+    scrollContainer.value.scrollTop = scrollContainer.value.scrollHeight;
+  }
+};
+
+watch(
+  messages,
+  () => {
+    void scrollToBottom();
+  },
+  { immediate: true },
+);
 </script>
